d98db624eb8d6955ddc9e962d298b27f1f915ad0
Javaspring-dev <jtraining32@gmail.com>
2024-12-17 19:36:43 +0530

    Add files via upload

---
+++ Cabbooking/carbooking/src/main/java/com/example/carbooking/entities/BookingEntity.java
@@ -0,0 +1,73 @@
+package com.example.carbooking.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Booking")
+public class BookingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookingid;
+    private int carid;
+    private int userid;
+    private int starttime;
+    private int endtime;
+    private String longitude;
+    private String latitude;
+
+    public int getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(int bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public int getCarid() {
+        return carid;
+    }
+
+    public void setCarid(int carid) {
+        this.carid = carid;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(int starttime) {
+        this.starttime = starttime;
+    }
+
+    public int getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(int endtime) {
+        this.endtime = endtime;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+}
---
+++ Cabbooking/carbooking/src/main/java/com/example/carbooking/repository/BookingRepository.java
@@ -0,0 +1,12 @@
+package com.example.carbooking.repository;
+
+import com.example.carbooking.entities.BookingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookingRepository extends JpaRepository<BookingEntity,Long> {
+
+}
